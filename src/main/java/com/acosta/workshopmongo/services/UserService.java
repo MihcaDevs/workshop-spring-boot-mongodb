572da5bdef66702535475b8defdf0b3f108a0012
Alberto Costa <mihcadevs@gmail.com>
2019-09-10 22:27:11 -0300

    Iserção de usuario com post

--- src/main/java/com/acosta/workshopmongo/services/UserService.java
+++ src/main/java/com/acosta/workshopmongo/services/UserService.java
@@ -6,24 +6,32 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.acosta.workshopmongo.config.dto.UserDTO;
 import com.acosta.workshopmongo.domain.User;
 import com.acosta.workshopmongo.exception.ObjectNotFoundException;
 import com.acosta.workshopmongo.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
-		
+
 	}
-	
-	public User findById(String id) { 
-		Optional<User> obj = repo.findById(id);  
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
