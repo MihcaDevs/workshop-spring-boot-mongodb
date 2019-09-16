4ff1e57aa8c33a209ee3bac7f6887cb418c63e53
Alberto Costa <mihcadevs@gmail.com>
2019-09-15 23:34:53 -0300

    consulta simples com query methods

--- src/main/java/com/acosta/workshopmongo/services/PostService.java
+++ src/main/java/com/acosta/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.acosta.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,4 +26,8 @@ public class PostService {
 	return user;
 	
 }
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 }
