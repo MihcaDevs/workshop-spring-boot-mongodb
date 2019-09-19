409ff2f9d1b59a9f016cb8863a7b3ddd454d5995
Alberto Costa <mihcadevs@gmail.com>
2019-09-18 22:57:55 -0300

    consulta com varios criterios

--- src/main/java/com/acosta/workshopmongo/services/PostService.java
+++ src/main/java/com/acosta/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.acosta.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -30,4 +31,12 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		return repo.serchTitle(text);
 }
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+
+
+
+	}
 }
